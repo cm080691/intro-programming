37164f61962d099cb4b7fbaf93c3945b859e5134
pradeep more <more08.chetan@gmail.com>
2016-07-07 01:15:01 +0000

    adding task script

--- assignment_3/save_task.sh
+++ assignment_3/save_task.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-newTask="pick up the coffee from starbucks"
-echo $newTask >> ~/task_database.txt
-
-newTask="when you see this the task text has been expanded"
-
-# double quotes are expanded, single are not
-echo $newTask
-echo "$newTask"
-echo '$newTask'
-
-echo "------------"
-echo "first arg: $1"
-echo "============"
-echo "first arg: $2"
-echo "============"
-echo "first arg: $3"
 
 newTask=$1
-echo "$newTask"
+echo $newTask >> ~/task_database.txt
+
+
+
